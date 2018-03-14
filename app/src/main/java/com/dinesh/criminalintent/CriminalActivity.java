c58eebd1d9fa5e1e409ab6da7b1c3dad842506cb
dinesh19aug@gmail.com <dinesh19aug@gmail.com>
2018-03-13 22:25:44 -0400

    Initial Commit for App Criminal Intent

---
+++ app/src/main/java/com/dinesh/criminalintent/CriminalActivity.java
@@ -0,0 +1,24 @@
+package com.dinesh.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CriminalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criminal);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if(fragment == null){
+            fragment = new FragmentCrime();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
